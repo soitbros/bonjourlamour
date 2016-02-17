ec8cd01fc6668fbf1cddaa59edabb7723a322b66
Clint Osterholz <clintosterholz@gmail.com>
2016-02-16 20:57:30 -0500

    fixes things for heroku deployment

--- client/controllers/navbar.js
+++ client/controllers/navbar.js
@@ -1,7 +1,5 @@
 app.controller('navbarCtrl', function($scope, $window, $auth, $location) {
 
-  $scope.username = JSON.parse(localStorage.getItem('currentUser')).username;
-
   $scope.isAuthenticated = function() {
     return $auth.isAuthenticated();
   };
