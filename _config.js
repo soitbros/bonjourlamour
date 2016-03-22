fedb0e7631cc354402579c7497ba1ded8f2ccbec
Clint Osterholz <clintosterholz@gmail.com>
2016-03-21 22:00:54 -0400

    ughh

--- _config.js
+++ _config.js
@@ -1,5 +1,5 @@
 module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'my-precious',
-  MONGOLAB_URI: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/bonbon-1',
+  MONGOLAB_URI: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/bonbon-2',
   SALT_WORK_FACTOR: 10,
 };
